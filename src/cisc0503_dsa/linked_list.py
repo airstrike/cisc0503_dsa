3257418907985666eddf9171ff7e6e4c2df83137
Andy Terra <andy@andyterra.com>
2023-11-15 19:28:39 -0500

    Add DoublyLinkedList, more tests

--- src/cisc0503_dsa/linked_list.py
+++ src/cisc0503_dsa/linked_list.py
@@ -2,18 +2,33 @@
 
 *   `Node` implements a base class for a node in a linked list.
 *   `LinkedList` implements a base class for a singly linked list with standard operations.
+*   `DoublyLinkedList` inherits from `LinkedList` to implement a doubly linked list with standard operations.
 """
 
 class Node:
     def __init__(self, element):
         self.element = element
         self.next: Node = None
+        self.prev: Node = None
+    
+    def __repr__(self): # pragma: no cover
+        return self.__str__()
+
+    def __prev__(self, with_self=True): # pragma: no cover
+        """Returns a string representation of the chain of previous nodes."""
+        return (f"{self.prev.__prev__()} <- " if self.prev else "") + (f"({self.element})" if with_self else "")
+
+    def __next__(self, with_self=True): # pragma: no cover
+        """Returns a string representation of the chain of next nodes."""
+        return (f"({self.element}) " if with_self else "") + (f"-> {self.next.__next__()}" if self.next else "")
     
     def __repr__(self): # pragma: no cover
         return self.__str__()
     
     def __str__(self): # pragma: no cover
-        return f"{self.__class__.__name__}({self.element})" + (f" -> {self.next}" if self.next else ".")
+        prev_str = self.__prev__(with_self=False)
+        next_str = self.__next__(with_self=False)
+        return f"{prev_str} Node({self.element}) {next_str}".strip()
 
 class LinkedList:
 
@@ -23,7 +38,7 @@ class LinkedList:
         self._size = 0
         self.node_class = Node
 
-    def node(self, element):
+    def make_node(self, element):
         return self.node_class(element)
     
     def __repr__(self): # pragma: no cover
@@ -69,7 +84,7 @@ class LinkedList:
         """Insert an element at the beginning of the list.
 
         """
-        node = self.node(element)
+        node = self.make_node(element)
         node.next = self._head
         self._head = node
         self._size += 1
@@ -80,7 +95,7 @@ class LinkedList:
         """Insert an element at the end of the list.
 
         """
-        node = self.node(element)
+        node = self.make_node(element)
         if self._tail is None:
             self._head = self._tail = node
         else:
@@ -101,7 +116,7 @@ class LinkedList:
             for i in range(1, index):
                 current = current.next
             temp = current.next
-            current.next = self.node(element)
+            current.next = self.make_node(element)
             current.next.next = temp
             self._size += 1
 
@@ -110,21 +125,21 @@ class LinkedList:
 
         """
         if self._size == 0:
-            return None
+            return False
         else:
             temp = self._head
             self._head = self._head.next
             self._size -= 1
             if self._head is None:
                 self._tail = None
-            return temp.element
+            return True
 
     def remove_last(self):
         """Remove the last element in the list.
 
         """
         if self._size == 0:
-            return None
+            return False
         elif self._size == 1:
             return self.remove_first()
         else:
@@ -135,14 +150,14 @@ class LinkedList:
             self._tail = current
             self._tail.next = None
             self._size -= 1
-            return temp.element
+            return True
 
     def remove(self, index):
         """Remove the element at the specified index in the list.
 
         """
         if index < 0 or index >= self._size:
-            return None
+            return False
         elif index == 0:
             return self.remove_first()
         elif index == self._size - 1:
@@ -154,10 +169,10 @@ class LinkedList:
             current = previous.next
             previous.next = current.next
             self._size -= 1
-            return current.element
+            return True
 
-    def update(self, index, element):
-        """Update the element at the specified index in the list.
+    def set(self, index, element):
+        """Set the element at the specified index in the list.
 
         """
         if index < 0 or index >= self._size:
@@ -206,10 +221,182 @@ class LinkedList:
         return self.get(index)
 
     def traverse(self):
-        """Traverse the list.
+        """Iterate over the elements in the list.
 
         """
         current = self._head
         while current:
             yield current.element
             current = current.next
+
+    def contains(self, element):
+        """Return True if the list contains the specified element, False otherwise.
+
+        """
+        for current in self:
+            if current == element:
+                return True
+        return False
+
+    def indexOf(self, element):
+        """Return the index of the first matching element in the list.
+
+        """
+        return self.index(element, reverse=False)
+
+    def lastIndexOf(self, element):
+        """Return the index of the last matching element in the list.
+
+        """
+        return self.index(element, reverse=True)
+
+    def index(self, element, reverse=False):
+        """Return the index of the first matching element in the list, searching
+        in the specified direction.
+
+        args:
+            element - the element to search for.
+            direction - the direction to traverse the list. 1 for forward, -1 for backward.
+        """
+        index = -1
+        for i, current in enumerate(self.traverse()):
+            if current == element and not reverse:
+                index = i
+                break
+            elif current == element and reverse:
+                index = i
+        return index
+
+
+class DoublyLinkedList(LinkedList):
+
+    def insert(self, index, element):
+        """Insert an element at the specified index in the list.
+
+        """
+        if index == 0:
+            self.insert_first(element)
+        elif index >= len(self):
+            self.insert_last(element)
+        else:
+            current = self._head
+            for i in range(1, index):
+                current = current.next
+            temp = current.next
+            current.next = self.make_node(element)
+            current.next.prev = current
+            current.next.next = temp
+            temp.prev = current.next
+            self._size += 1
+
+    def insert_first(self, index):
+        """Insert an element at the beginning of the list.
+
+        """
+        node = self.make_node(index)
+        node.next = self._head
+        self._head = node
+        self._size += 1
+        if self._tail is None:
+            self._tail = self._head
+
+    def insert_last(self, index):
+        """Insert an element at the end of the list.
+
+        """
+        node = self.make_node(index)
+        if self._tail is None:
+            self._head = self._tail = node
+        else:
+            self._tail.next = node
+            node.prev = self._tail
+            self._tail = self._tail.next
+        self._size += 1
+
+    def remove(self, index):
+        """Remove an element from the list and update references
+
+        """
+        if index < 0 or index >= self._size:
+            return False
+        if index == 0:
+            return self.remove_first()
+        if index == self._size - 1:
+            return self.remove_last()
+        else:
+            current = self._head
+            for i in range(1, index):
+                current = current.next
+            current.next = current.next.next
+            current.next.prev = current
+            self._size -= 1
+            return True
+    
+    def remove_first(self):
+        """Remove the first element in the list.
+
+        """
+        if self._size == 0:
+            return False
+        else:
+            temp = self._head
+            self._head = self._head.next
+            self._size -= 1
+            if self._head is None:
+                self._tail = None
+            return True
+
+    def remove_last(self):
+        """Remove the last element in the list.
+
+        """
+        if self._size == 0:
+            return False
+        elif self._size == 1:
+            return self.remove_first()
+        else:
+            temp = self._tail
+            self._tail = self._tail.prev
+            self._tail.next = None
+            self._size -= 1
+            return True
+
+    def traverse(self, reverse=False):
+        """Iterate over the elements in the list.
+
+        """
+        if reverse:
+            current = self._tail
+            while current:
+                yield current.element
+                current = current.prev
+        else:
+            current = self._head
+            while current:
+                yield current.element
+                current = current.next
+
+    def __iter__(self):
+        return self.traverse()
+
+    def index(self, element, reverse=False):
+        """Return the index of the first matching element in the list, searching
+        in the specified direction.
+
+        args:
+            element - the element to search for.
+            direction - the direction to traverse the list. 1 for forward, -1 for backward.
+        """
+
+        index = -1
+        for i, current in enumerate(self.traverse(reverse=reverse)):
+            if current == element:
+                index = self._size - i - 1 if reverse else i
+                break
+        return index
+
+    def lastIndexOf(self, element):
+        """Return the index of the last matching element in the list.
+
+        """
+        return self.index(element, reverse=True)
--- tests/test_LinkedList.py
+++ tests/test_LinkedList.py
@@ -5,7 +5,7 @@ import unittest
 # Add the path to the src directory to the system path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/src')
 
-from cisc0503_dsa import LinkedList
+from cisc0503_dsa import LinkedList, DoublyLinkedList
 
 class LinkedListTest(unittest.TestCase):
     """Test the LinkedList class"""
@@ -14,6 +14,9 @@ class LinkedListTest(unittest.TestCase):
         self.ll = LinkedList()
         for i in range(10):
             self.ll.insert_last(i)
+
+        # modify the 2nd item to equal the first
+        self.ll.set(1, 0)
         
         self.empty_ll = LinkedList()
 
@@ -28,8 +31,14 @@ class LinkedListTest(unittest.TestCase):
         self.assertEqual(self.ll.get_last(), 100)
 
     def test_traverse(self):
-        self.assertEqual([node for node in self.ll.traverse()], [0,1,2,3,4,5,6,7,8,9])
-        self.assertEqual([node for node in self.ll], [0,1,2,3,4,5,6,7,8,9])
+        self.assertEqual(len([node for node in self.ll.traverse()]), self.ll.get_size())
+        self.assertEqual([node for node in self.ll.traverse()], [0,0,2,3,4,5,6,7,8,9])
+        self.assertEqual([node for node in self.ll], [0,0,2,3,4,5,6,7,8,9])
+
+        self.ll.remove(4)
+        self.assertEqual(len([node for node in self.ll.traverse()]), self.ll.get_size())
+        self.assertEqual([node for node in self.ll.traverse()], [0,0,2,3,5,6,7,8,9])
+        self.assertEqual([node for node in self.ll], [0,0,2,3,5,6,7,8,9])
 
     def test_get_size(self):
         self.assertEqual(self.ll.get_size(), 10)
@@ -66,7 +75,7 @@ class LinkedListTest(unittest.TestCase):
     def test_remove(self):
         self.ll.remove(0)
         self.assertEqual(self.ll.get_size(), 9)
-        self.assertEqual(self.ll.get_first(), 1)
+        self.assertEqual(self.ll.get_first(), 0)
 
         self.ll.remove(8) # actually removes the element with value `9`
         self.assertEqual(self.ll.get_size(), 8)
@@ -77,32 +86,32 @@ class LinkedListTest(unittest.TestCase):
         self.assertEqual(self.ll.get(4), 6)
 
         # test calling remove() at the first index
-        self.ll.remove(0)
+        self.assertTrue(self.ll.remove(0))
 
         # test calling remove() at the last element
-        self.ll.remove(6)
+        self.assertTrue(self.ll.remove(len(self.ll) - 1))
 
         # test calling remove*() on first/last index of empty list
-        self.assertIsNone(self.empty_ll.remove(0))
-        self.assertIsNone(self.empty_ll.remove_first())
-        self.assertIsNone(self.empty_ll.remove_last())
+        self.assertFalse(self.empty_ll.remove(0))
+        self.assertFalse(self.empty_ll.remove_first())
+        self.assertFalse(self.empty_ll.remove_last())
 
         # test calling remove_last() on single element list
         self.empty_ll.insert_last(100)
-        self.assertEqual(self.empty_ll.remove_last(), 100)
+        self.assertTrue(self.empty_ll.remove_last())
 
-    def test_update(self):
-        self.ll.update(0, 100)
+    def test_set(self):
+        self.ll.set(0, 100)
         self.assertEqual(self.ll.get(0), 100)
 
-        self.ll.update(9, 100)
+        self.ll.set(9, 100)
         self.assertEqual(self.ll.get(9), 100)
 
-        self.ll.update(4, 100)
+        self.ll.set(4, 100)
         self.assertEqual(self.ll.get(4), 100)
 
-        # test calling update() on empty list
-        self.empty_ll.update(0, 100)
+        # test calling set() on empty list
+        self.empty_ll.set(0, 100)
 
     def test_get(self):
         self.assertEqual(self.ll.get(0), 0)
@@ -124,18 +133,86 @@ class LinkedListTest(unittest.TestCase):
         self.assertIsNone(self.empty_ll.get_last())
 
     def test_equality(self):
-        equal = LinkedList()
+        mostly_equal = LinkedList()
         for i in range(10):
-            equal.insert_last(i)
-        self.assertEqual(self.ll, equal)
-        equal.update(0, 100)
-        self.assertNotEqual(self.ll, equal)
+            mostly_equal.insert_last(i)
+        self.assertNotEqual(self.ll, mostly_equal)
+        mostly_equal.set(1, 0) # make the 2nd item equal to the first
+        self.assertEqual(self.ll, mostly_equal)
 
         self.assertEqual(self.empty_ll, LinkedList())
         self.assertNotEqual(self.ll, self.empty_ll)
 
-
     def test_copies(self):
         self.assertEqual(self.ll.copy(), self.ll)
 
+    def test_insertions_change_size(self):
+        self.assertEqual(self.ll.get_size(), 10)
+        self.ll.insert_first(100)
+        self.assertEqual(self.ll.get_size(), 11)
+        self.ll.insert_last(100)
+        self.assertEqual(self.ll.get_size(), 12)
+        self.ll.insert(0, 100)
+        self.assertEqual(self.ll.get_size(), 13)
+        self.ll.insert(5, 100)
+        self.assertEqual(self.ll.get_size(), 14)
 
+    def test_removals_change_size(self):
+        self.assertEqual(self.ll.get_size(), 10)
+        self.ll.remove(0)
+        self.assertEqual(self.ll.get_size(), 9)
+        self.ll.remove(8)
+        self.assertEqual(self.ll.get_size(), 8)
+        self.ll.remove(4)
+        self.assertEqual(self.ll.get_size(), 7)
+
+    def test_contains(self):
+        self.assertTrue(self.ll.contains(0))
+        self.assertTrue(self.ll.contains(9))
+        self.assertTrue(self.ll.contains(4))
+        self.assertFalse(self.ll.contains(100))
+        self.assertFalse(self.empty_ll.contains(100))
+    
+    def test_index(self):
+        self.assertEqual(self.ll.index(0), 0)
+        self.assertEqual(self.ll.index(9), 9)
+        self.assertEqual(self.ll.index(4), 4)
+        self.assertEqual(self.ll.index(100), -1)
+        self.assertEqual(self.empty_ll.index(100), -1)
+
+        # test indexOf
+        self.assertEqual(self.ll.indexOf(0), self.ll.index(0))
+
+        # test index in opposite direction
+        self.assertEqual(self.ll.index(0, reverse=True), 1)
+
+    def test_last_indexOf(self):
+        self.assertEqual(self.ll.lastIndexOf(0), 1)
+        self.assertEqual(self.ll.lastIndexOf(9), 9)
+        self.assertEqual(self.ll.lastIndexOf(4), 4)
+        self.assertEqual(self.ll.lastIndexOf(100), -1)
+        self.assertEqual(self.empty_ll.lastIndexOf(100), -1)
+
+class DoublyLinkedListTest(LinkedListTest):
+    """Inherits from LinkedListTest and overrides the setUp() method to use a DoublyLinkedList
+    """
+
+    def setUp(self):
+        self.ll = DoublyLinkedList()
+        for i in range(10):
+            self.ll.insert_last(i)
+
+        # modify the 2nd item to equal the first
+        self.ll.set(1, 0)
+        
+        self.empty_ll = DoublyLinkedList()
+
+    def test_traverse_reverse(self):
+        self.assertEqual(len([node for node in self.ll.traverse(reverse=True)]), self.ll.get_size())
+        self.assertEqual([node for node in self.ll.traverse(reverse=True)], [9,8,7,6,5,4,3,2,0,0])
+        self.assertEqual([node for node in self.ll], [0,0,2,3,4,5,6,7,8,9])
+
+        self.ll.remove(4)
+        self.assertEqual(len([node for node in self.ll.traverse(reverse=True)]), self.ll.get_size())
+        self.assertEqual([node for node in self.ll.traverse(reverse=True)], [9,8,7,6,5,3,2,0,0])
+        self.assertEqual([node for node in self.ll], [0,0,2,3,5,6,7,8,9])
